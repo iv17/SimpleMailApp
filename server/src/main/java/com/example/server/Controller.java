57eb02202f0c988bb903509f5a53e23f6ac0b99b
iv17 <ivana.unitedforce@gmail.com>
2019-03-16 17:31:25 +0100

    conected

--- server/src/main/java/com/example/server/Controller.java
+++ server/src/main/java/com/example/server/Controller.java
@@ -1,17 +1,17 @@
 package com.example.server;
 
-import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-
-import javax.servlet.http.HttpServletRequest;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -35,11 +35,13 @@ import com.google.api.services.gmail.model.ListLabelsResponse;
 import com.google.api.services.gmail.model.ListMessagesResponse;
 import com.google.api.services.gmail.model.Message;
 
-
+//@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:5500")
 @RestController
+@RequestMapping(value = "")
 public class Controller {
 
-	private static final String APPLICATION_NAME = "GmailAlexa";
+	private static final String APPLICATION_NAME = "Simple mail app";
 	private static HttpTransport httpTransport;
 	private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
 	private static Gmail client;
@@ -56,60 +58,45 @@ public class Controller {
 
 	@Value("${gmail.client.redirectUri}")
 	private String redirectUri;
-
+	
 	@RequestMapping(value = "/login/gmail", method = RequestMethod.GET)
-	public RedirectView googleConnectionStatus(HttpServletRequest request) throws Exception {
-		return new RedirectView(authorize());
-	}
-
-	@RequestMapping(value = "/login/gmailCallback2", method = RequestMethod.GET, params = "code")
-	public ResponseEntity<String> getMessages(@RequestParam(value = "code") String code) {
-
-		JSONObject json = new JSONObject();
-		JSONArray arr = new JSONArray();
-
-
-		try {
-			TokenResponse response = flow.newTokenRequest(code).setRedirectUri(redirectUri).execute();
-			credential = flow.createAndStoreCredential(response, "userID");
-
-			client = new com.google.api.services.gmail.Gmail.Builder(httpTransport, JSON_FACTORY, credential)
-					.setApplicationName(APPLICATION_NAME).build();
-
-
-			String userId = "me";
-			ListMessagesResponse msgResponse = client.users().messages().list(userId).execute();
-
-			List<Message> messages = new ArrayList<>();
-
-			for (Message msg : msgResponse.getMessages()) {
-
-				messages.add(msg);
-				Message message = client.users().messages().get(userId, msg.getId()).execute();
-				arr.put(message.getSnippet());
-
-			}
-			json.put("response", arr);
-
-			for (Message msg : messages) {
-
-				System.out.println("msg: " + msg.toPrettyString());
-			}
-
-		} catch (Exception e) {
-
-			System.out.println("exception cached ");
-			e.printStackTrace();
+	public RedirectView googleConnectionStatus() throws Exception {
+		AuthorizationCodeRequestUrl authorizationUrl;
+		if (flow == null) {
+			Details web = new Details();
+			web.setClientId(clientId);
+			web.setClientSecret(clientSecret);
+			clientSecrets = new GoogleClientSecrets().setWeb(web);
+			httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+			
+			flow = new GoogleAuthorizationCodeFlow.Builder(httpTransport, JSON_FACTORY, clientSecrets,
+					Collections.singleton(GmailScopes.GMAIL_READONLY)).build();
 		}
+		authorizationUrl = flow.newAuthorizationUrl().setRedirectUri(redirectUri);
 
-		return new ResponseEntity<>(json.toString(), HttpStatus.OK);
+		String build = authorizationUrl.build();
+	
+		return new RedirectView(build);
+	}
+	
+	@RequestMapping(value = "/login/gmailCallback", method = RequestMethod.GET, params = "code",	
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public RedirectView callback(@RequestParam(value = "code") String code) throws URISyntaxException {
+		
+		RedirectView redirect = new RedirectView("http://localhost:5500/SimpleMailApp/client/index.html");
+		redirect.addStaticAttribute("code", code);
+		return redirect;
 
 	}
 	
-	@RequestMapping(value = "/login/gmailCallback", method = RequestMethod.GET, params = "code")
-	public ResponseEntity<String> listLabels(@RequestParam(value = "code") String code) throws IOException {
-		
+	@RequestMapping(value = "/login/messages", method = RequestMethod.GET, params = "code",
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<String> getMessages(@RequestParam(value = "code") String code) {
+
+		System.out.println(code);
 		JSONObject json = new JSONObject();
+		JSONArray arr = new JSONArray();
+
 
 		try {
 			TokenResponse response = flow.newTokenRequest(code).setRedirectUri(redirectUri).execute();
@@ -122,33 +109,27 @@ public class Controller {
 			ListLabelsResponse labelsResponse = client.users().labels().list(userId).execute();
 			List<Label> labels = labelsResponse.getLabels();
 			
-			json.put("response", labels);
+			json.put("labels", labels);
+			
+			ListMessagesResponse msgResponse = client.users().messages().list(userId).execute();
 
-		
+			List<Message> messages = new ArrayList<>();
+			for (Message msg : msgResponse.getMessages()) {
+
+				messages.add(msg);
+				Message message = client.users().messages().get(userId, msg.getId()).execute();
+				arr.put(message.getSnippet());
+			}
+			json.put("messages", arr);
+			System.out.println(json);
+
+
 		} catch (Exception e) {
-
-			System.out.println("exception cached ");
 			e.printStackTrace();
 		}
 
+		
 		return new ResponseEntity<>(json.toString(), HttpStatus.OK);
-	  
-	  }
-	
-	private String authorize() throws Exception {
-		AuthorizationCodeRequestUrl authorizationUrl;
-		if (flow == null) {
-			Details web = new Details();
-			web.setClientId(clientId);
-			web.setClientSecret(clientSecret);
-			clientSecrets = new GoogleClientSecrets().setWeb(web);
-			httpTransport = GoogleNetHttpTransport.newTrustedTransport();
-			flow = new GoogleAuthorizationCodeFlow.Builder(httpTransport, JSON_FACTORY, clientSecrets,
-					Collections.singleton(GmailScopes.GMAIL_READONLY)).build();
-		}
-		authorizationUrl = flow.newAuthorizationUrl().setRedirectUri(redirectUri);
 
-		return authorizationUrl.build();
 	}
-
 }
