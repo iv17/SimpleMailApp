b0ba4617673e7f05c4e65f4adf4b933f182ceaf2
iv17 <ivana.unitedforce@gmail.com>
2019-04-06 17:51:00 +0200

    show single mail

--- client/drawings/drawSingleMail.js
+++ client/drawings/drawSingleMail.js
@@ -31,7 +31,7 @@ export default function drawSingleMail(message) {
     var vp13 = new VerticalPanel('vp13', 'compose-btn pull-left');
     vp12.add(vp13);
 
-    var button1 = new AContainer('button1', 'btn btn-sm btn-primary', 'Reply', '#', '');
+    var button1 = new AContainer('button1', 'btn btn-sm btn-primary', 'Reply ', '#', '');
     vp13.add(button1);
     var i1 = new I('i1', 'fa fa-reply');
     button1.add(i1);
@@ -39,7 +39,7 @@ export default function drawSingleMail(message) {
     var emptyCol1 = new EmptyCol('ec1', '');
     vp13.add(emptyCol1);
 
-    var button2 = new AContainer('button2', 'btn btn-sm btn-default', 'Forward', '#', '');
+    var button2 = new AContainer('button2', 'btn btn-sm btn-default', 'Forward ', '#', '');
     vp13.add(button2);
     var i2 = new I('i2', 'fa fa-arrow-right');
     button2.add(i2);
@@ -47,7 +47,7 @@ export default function drawSingleMail(message) {
     var emptyCol2 = new EmptyCol('ec2', '');
     vp13.add(emptyCol2);
 
-    var button3 = new AContainer('button3', 'btn btn-sm btn-default', 'Delete', '#', '');
+    var button3 = new AContainer('button3', 'btn btn-sm btn-default', 'Delete ', '#', '');
     vp13.add(button3);
     var i3 = new I('i3', 'fa fa-trash-o');
     button3.add(i3);
@@ -64,7 +64,7 @@ export default function drawSingleMail(message) {
     var vp15 = new VerticalPanel('vp15', 'col-md-12');
     vp11.add(vp15);
 
-    var h41 = new H4('h41', '', message.subject);
+    var h41 = new H4('h41', '', subject);
     vp15.add(h41);
     var hr1 = new HR('hr1', '');
     vp15.add(hr1);
@@ -81,11 +81,11 @@ export default function drawSingleMail(message) {
     var vp18 = new VerticalPanel('vp18', 'col-md-12');
     vp17.add(vp18);
 
-    var senderImage = new Image('senderImage', '', 'https://bootdey.com/img/Content/avatar/avatar6.png', sender.name, '40px', '40px');
+    var senderImage = new Image('senderImage', '', 'https://bootdey.com/img/Content/avatar/avatar6.png', from, '40px', '40px');
     vp18.add(senderImage);
     var senderName = new Strong('senderName', '', from);
     vp18.add(senderName);
-    var senderMail = new Label('senderMail', '', ' [' + from + ' ] ');
+    var senderMail = new Label('senderMail', '', ' [' + from + '] ');
     vp18.add(senderMail);
     var textTo = new Label('textTo', '', ' to ');
     vp18.add(textTo);
@@ -104,21 +104,21 @@ export default function drawSingleMail(message) {
     var vp20 = new VerticalPanel('vp20', 'compose-btn pull-left');
     vp10.add(vp20);
 
-    var button4 = new AContainer('button4', 'btn btn-sm btn-primary', 'Reply', '', '');
+    var button4 = new AContainer('button4', 'btn btn-sm btn-primary', 'Reply ', '', '');
     vp20.add(button4);
     var i4 = new I('i4', 'fa fa-reply');
     button4.add(i4);
     var emptyCol3 = new EmptyCol('ec3', '');
     vp20.add(emptyCol3);
 
-    var button5 = new AContainer('button5', 'btn btn-sm btn-default', 'Forward', '', '');
+    var button5 = new AContainer('button5', 'btn btn-sm btn-default', 'Forward ', '', '');
     vp20.add(button5);
     var i5 = new I('i5', 'fa fa-arrow-right');
     button5.add(i5);
     var emptyCol4 = new EmptyCol('ec4', '');
     vp20.add(emptyCol4);
 
-    var button6 = new AContainer('button6', 'btn btn-sm btn-default', 'Delete', '', '');
+    var button6 = new AContainer('button6', 'btn btn-sm btn-default', 'Delete ', '', '');
     vp20.add(button6);
     var i6 = new I('i6', 'fa fa-trash-o');
     button6.add(i6);
