8e47db6cbe7450630d9744aa8529bfff8d4f668b
iv17 <ivana.unitedforce@gmail.com>
2019-04-20 18:14:35 +0200

    create draft

--- client/drawings/forward/drawForward.js
+++ client/drawings/forward/drawForward.js
@@ -3,9 +3,10 @@ import {
     InputArea, InputField, Strong, Label, AContainer, ButtonContainer,
     Form, HorizontalPanel, LI, MainPanel, UL, VerticalPanel
 } from '../../js/osc.js';
+import drawForwardButtons from './drawForwardButtons.js';
 
 export default function drawForward(message) {
-
+   
     var vp9 = new VerticalPanel('vp9', 'col-sm-9 col-md-10');
 
     var vp10 = new VerticalPanel('vp10', 'inbox-body');
@@ -23,54 +24,17 @@ export default function drawForward(message) {
 
     var vp13 = new VerticalPanel('vp13', 'view-mail');
     vp10.add(vp13);
-    var inputTitle = new InputField('inputTitle', 'form-control', 'text', '');
+    var inputTitle = new InputField('inputTitle', 'form-control', 'text', message.headers.subject);
     vp13.add(inputTitle);
     var emptyRow2 = new EmptyRow('er2', '');
     vp13.add(emptyRow2);
-    var inputMessage = new InputArea('inputMessage', 'form-control', '', 10, 50);
+    var inputMessage = new InputArea('inputMessage', 'form-control', message.content, 10, 50);
     vp13.add(inputMessage);
     var emptyRow3 = new EmptyRow('er3', '');
     vp13.add(emptyRow3);
 
-    document.getElementById('inputTitle').value = message.headers.subject;
-    document.getElementById('inputMessage').value = message.content;
-
-    var vp14 = new VerticalPanel('vp14', 'compose-btn pull-left');
+    var vp14 = drawForwardButtons(message);
     vp10.add(vp14);
-
-    var buttonSend = new AContainer('buttonSend', 'btn btn-sm btn-primary', 'Send ', '', '');
-    vp14.add(buttonSend);
-    var i1 = new I('i1', 'fa fa-envelope');
-    buttonSend.add(i1);
-    var ec1 = new EmptyCol('emptyCol1', '');
-    vp14.add(ec1);
-    
-    buttonSend.onclick = function (e) {
-        e.preventDefault();
-        e.stopImmediatePropagation();
-
-        var to = document.getElementById('inputEmail').value;
-        var subject = document.getElementById('inputTitle').value;
-        var bodyText = document.getElementById('inputMessage').value;
-        
-    }
-
-    var buttonDiscard = new AContainer('buttonDiscard', 'btn btn-sm btn-default', 'Discard ', '', '');
-    vp14.add(buttonDiscard);
-    var i2 = new I('i2', 'fa fa-trash-o');
-    buttonDiscard.add(i2);
-    var ec2 = new EmptyCol('emptyCol2', '');
-    vp14.add(ec2);
-
-    buttonDiscard.onclick = function (e) {
-        e.preventDefault();
-        e.stopImmediatePropagation();
-        console.log('DISCARD');
-        
-    }
-
-    var hr2 = new HR('hr2', '');
-    vp14.add(hr2);
 
     return vp9;
 }
