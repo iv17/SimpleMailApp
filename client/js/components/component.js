80b186180059cbb4e73fd97dc11d2f0495d33194
iv17 <ivana.unitedforce@gmail.com>
2019-04-27 14:52:15 +0200

    bug fix

--- client/js/components/component.js
+++ client/js/components/component.js
@@ -2,22 +2,10 @@ export default class Component {
   constructor(id, CSSclass) {
     this.id = id;
     this.CSSclass = CSSclass;
-
     this.node = document.createElement("div");
     this.node.id = id;
     this.node.component = this;
     document.body.appendChild(this.node);
-
-  }
-  findById(id) {
-    var parent = this.getParent();
-    
-    for (var child of parent.children.values()) {
-      if (child.id == id) {
-        return child;
-      }
-    }
-    return parent.findById(id);
   }
 
   setParent(parent) {
@@ -40,6 +28,16 @@ export default class Component {
   removeCSSClass(CSSclass) {
     this.CSSclass = '';
     $('#' + this.id).removeClass(CSSclass);
+  }
+
+  findById(id) {
+    var parent = this.getParent();
+    for (var child of parent.children.values()) {
+      if (child.id == id) {
+        return child;
+      }
+    }
+    return parent.findById(id);
   }
 
   tohtml() {
